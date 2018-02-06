f6c6788e2719d43440b7179d55a78f4410097ef1
Fioletibiel <pawel89kaminski@gmail.com>
2018-02-06 15:43:53 +0100

    Próba poprawienia ruchu ludzi.

--- 9.3.yeah^11.py
+++ 9.3.yeah^11.py
@@ -85,27 +85,39 @@ l=0
 while(l < L):
     x = np.random.randint(0,X)
     y = np.random.randint(0,Y)
-    if(pole[y][x]!='©'):
+    if(pole[y][x]==''):
         pole[y][x] = str(np.random.randint(0,2))
         if(pole[y][x]=='0'):
             pole[y][x]=''
         else:
             pole[y][x]='©'
             l+=1
-print("Pole przed przetasowaniem:")
-print(np.matrix(pole))
-print("\n")
+# print("Pole przed przetasowaniem:")
+# print(np.matrix(pole))
+# print("\n")
 
 # stworzenie początkowej tabeli rozmieszczenia ludzi (miejsca pola, w których są ludzie)
 tabela_wspolrzednych_ludzi = [[0 for i in range(3)] for l in range(L)]
 l=0
 for y in range(Y):
     for x in range(X):
-        if(pole[y][x]=='©'):
+        if(pole[y][x]!=''):
             tabela_wspolrzednych_ludzi[l]=[l+1,x+1,y+1]
             l+=1
 print("Tabela współrzędnych ludzi przed przetasowaniem:")
 print(np.matrix(tabela_wspolrzednych_ludzi))
+print("\n")
+
+# odtworzenie tabeli rozmieszczenia ludzi
+pole = [['' for x in range(X)] for y in range(Y)]
+for y in range(Y):
+    for x in range(X):
+        for lp in range(L):
+            if (tabela_wspolrzednych_ludzi[lp][1] - 1 == x):
+                if (tabela_wspolrzednych_ludzi[lp][2] - 1 == y):
+                    pole[y][x] = str(lp + 1)
+print("Pole przed przetasowaniem:")
+print(np.matrix(pole))
 print("\n")
 
 # symulacja ruchu ludzi
@@ -116,8 +128,8 @@ wszyscy_na_miejscu_lol='falsz'
 while(wszyscy_na_miejscu_lol!='prawda'):
 
     for i in range(L):
-        tabela_wektorow[i][0] = tabela_wspolrzednych_ludzi[tabela_wyboru[i][1] - 1][1] - 1 - (tabela_wspolrzednych_ludzi[i][1] - 1) # przesunięcie w osi X - 1
-        tabela_wektorow[i][1] = tabela_wspolrzednych_ludzi[tabela_wyboru[i][1] - 1][2] - 1 - (tabela_wspolrzednych_ludzi[i][2] - 1) # przesunięcie w osi Y - 1
+        tabela_wektorow[i][0] = tabela_wspolrzednych_ludzi[tabela_wyboru[i][1] - 1][1] - 1 - (tabela_wspolrzednych_ludzi[i][1] - 1 - 1) # przesunięcie w osi X - 1
+        tabela_wektorow[i][1] = tabela_wspolrzednych_ludzi[tabela_wyboru[i][1] - 1][2] - 1 - (tabela_wspolrzednych_ludzi[i][2] - 1 - 1) # przesunięcie w osi Y - 1
 
         if(tabela_wspolrzednych_ludzi[tabela_wyboru[i][1] - 1][1]-2 < tabela_wspolrzednych_ludzi[i][1] < tabela_wspolrzednych_ludzi[tabela_wyboru[i][1] - 1][1]+2):
             wszyscy_na_miejscu[i]='prawda'
@@ -126,14 +138,17 @@ while(wszyscy_na_miejscu_lol!='prawda'):
                     if(tabela_wspolrzednych_ludzi[i][1] == tabela_wspolrzednych_ludzi[otaa][1]):
                         wszyscy_na_miejscu[i]='falsz'
             if(wszyscy_na_miejscu[i]=='falsz'):
-                los = np.random.randint(0, 2)
-                if (los == 0):
+                if(tabela_wektorow[i][0] < 0):
                     los = -1
-                while(tabela_wspolrzednych_ludzi[i][1]+los<1 or tabela_wspolrzednych_ludzi[i][1]+los>X):
-                    los = np.random.randint(0, 2)
-                    if (los == 0):
-                        los = -1
-                tabela_wspolrzednych_ludzi[i][1] = tabela_wspolrzednych_ludzi[i][1]+los
+                if(tabela_wektorow[i][0] > 0):
+                    los = 1
+                if(tabela_wektorow[i][0] == 0):
+                    los = 0
+                tabela_wspolrzednych_ludzi[i][1] = tabela_wspolrzednych_ludzi[i][1] + los
+                if(tabela_wspolrzednych_ludzi[i][1]<1):
+                    tabela_wspolrzednych_ludzi[i][1] = tabela_wspolrzednych_ludzi[i][1]+1
+                if(tabela_wspolrzednych_ludzi[i][1] > X):
+                    tabela_wspolrzednych_ludzi[i][1] = tabela_wspolrzednych_ludzi[i][1]-1
         else:
             wszyscy_na_miejscu[i]='falsz'
             tabela_wspolrzednych_ludzi[i][1]=tabela_wspolrzednych_ludzi[i][1]+tabela_wektorow[i][0]/abs(tabela_wektorow[i][0]) # poruszanie się o 1 pole w kierunku wybranej osoby (w osi X)
@@ -159,14 +174,17 @@ while(wszyscy_na_miejscu_lol!='prawda'):
                     if(tabela_wspolrzednych_ludzi[i][2] == tabela_wspolrzednych_ludzi[otaa][2]):
                         wszyscy_na_miejscu[i]='falsz'
             if(wszyscy_na_miejscu[i]=='falsz'):
-                los = np.random.randint(0, 2)
-                if (los == 0):
+                if(tabela_wektorow[i][1] < 0):
                     los = -1
-                while(tabela_wspolrzednych_ludzi[i][2]+los<1 or tabela_wspolrzednych_ludzi[i][2]+los>Y):
-                    los = np.random.randint(0, 2)
-                    if (los == 0):
-                        los = -1
-                tabela_wspolrzednych_ludzi[i][2] = tabela_wspolrzednych_ludzi[i][2]+los
+                if(tabela_wektorow[i][1] > 0):
+                    los = 1
+                if(tabela_wektorow[i][1] == 0):
+                    los = 0
+                tabela_wspolrzednych_ludzi[i][2] = tabela_wspolrzednych_ludzi[i][2] + los
+                if(tabela_wspolrzednych_ludzi[i][2]<1):
+                    tabela_wspolrzednych_ludzi[i][2] = tabela_wspolrzednych_ludzi[i][1]+1
+                if(tabela_wspolrzednych_ludzi[i][2] > Y):
+                    tabela_wspolrzednych_ludzi[i][2] = tabela_wspolrzednych_ludzi[i][1]-1
         else:
             wszyscy_na_miejscu[i]='falsz'
             tabela_wspolrzednych_ludzi[i][2]=tabela_wspolrzednych_ludzi[i][2]+tabela_wektorow[i][1]/abs(tabela_wektorow[i][1]) # poruszanie się o 1 pole w kierunku wybranej osoby (w osi Y)
@@ -190,21 +208,34 @@ while(wszyscy_na_miejscu_lol!='prawda'):
         if (wszyscy_na_miejscu[lol]=='falsz'):
             wszyscy_na_miejscu_lol='falsz'
 
-    # odtworzenie tabeli rozmieszczenia ludzi / stworzenie końcowej tabeli rozmieszczenia ludzi
-    pole = [['' for x in range(X)] for y in range(Y)]
-    for y in range(Y):
-        for x in range(X):
-            for lp in range(L):
-                if (tabela_wspolrzednych_ludzi[lp][1] - 1 == x):
-                    if (tabela_wspolrzednych_ludzi[lp][2] - 1 == y):
-                        pole[y][x] = str(lp+1)
+    print(k+1)
+    k+1
+    
+    # # odtworzenie tabeli rozmieszczenia ludzi / stworzenie końcowej tabeli rozmieszczenia ludzi
+    # pole = [['' for x in range(X)] for y in range(Y)]
+    # for y in range(Y):
+    #     for x in range(X):
+    #         for lp in range(L):
+    #             if (tabela_wspolrzednych_ludzi[lp][1] - 1 == x):
+    #                 if (tabela_wspolrzednych_ludzi[lp][2] - 1 == y):
+    #                     pole[y][x] = str(lp+1)
 
     # odswiez_ekran()
-    # poczekaj(3000)
+    # poczekaj(3)
     # k += 1
     # print("Pole po przetasowaniu nr.:" + str(k))
     # print(np.matrix(pole))
     # print("\n")
+
+# odtworzenie tabeli rozmieszczenia ludzi / stworzenie końcowej tabeli rozmieszczenia ludzi
+pole = [['' for x in range(X)] for y in range(Y)]
+for y in range(Y):
+    for x in range(X):
+        for lp in range(L):
+            if (tabela_wspolrzednych_ludzi[lp][1] - 1 == x):
+                if (tabela_wspolrzednych_ludzi[lp][2] - 1 == y):
+                    pole[y][x] = str(lp+1)
+
 
 print("Pole po ostatecznym przetasowaniu:")
 print(np.matrix(pole))
@@ -213,4 +244,4 @@ print("Tabela współrzędnych ludzi po przetasowaniu:")
 print(np.matrix(tabela_wspolrzednych_ludzi))
 print("\n")
 
-input("Wciśnij cokolwiek, aby zakończyć program.")
+input("Wciśnij cokolwiek, aby zakończyć program ^^")
